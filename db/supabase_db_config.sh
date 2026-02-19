9f6e61b2950a66165a0032ea4e7916bfb47689e5
tony <bluein007@gmail.com>
2026-02-20 06:57:13 +0900

    DB Backup & 신규 계정에 Restore

--- db/supabase_db_config.sh
+++ db/supabase_db_config.sh
@@ -7,18 +7,18 @@ get_db_config() {
     case $PROJECT in
         "vision.echoit.co.kr")
             DB_HOST="aws-1-ap-northeast-2.pooler.supabase.com"
-            DB_PORT="5432"  # ← Session pooler
+            DB_PORT="5432"  # ← 강병우 계정 Session pooler
             DB_DATABASE="postgres"
             DB_USER="postgres.fivsbshaikmfjbbxkzsw"
             DB_PASS="fluAGocL20YYJ7CW"
             DB_POOLMODE="session"
             ;;
-        "www.telepasi.com")
-            DB_HOST=""
+        "new.vision.echoit.co.kr")
+            DB_HOST="aws-1-ap-northeast-2.pooler.supabase.com"      # - 에코아이티 계정
             DB_PORT="5432"  # ← Session pooler
             DB_DATABASE="postgres"
-            DB_USER="postgres.xxxxxxxxxxxxxxxxxxxx"
-            DB_PASS="xxxxxxxxxx"
+            DB_USER="postgres.yujniegzuxtyuasrbnvy"
+            DB_PASS="DZiF3eWwyA1fn27C"
             DB_POOLMODE="session"
             ;;
         *)
