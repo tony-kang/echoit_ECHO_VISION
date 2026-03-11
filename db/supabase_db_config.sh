11abc1073de83d417a1ff799c919ffcd837ccdb8
tony <bluein007@gmail.com>
2026-03-11 23:45:03 +0900

    DB Backup

--- db/supabase_db_config.sh
+++ db/supabase_db_config.sh
@@ -6,14 +6,6 @@ get_db_config() {
     
     case $PROJECT in
         "vision.echoit.co.kr")
-            DB_HOST="aws-1-ap-northeast-2.pooler.supabase.com"
-            DB_PORT="5432"  # ← 강병우 계정 Session pooler
-            DB_DATABASE="postgres"
-            DB_USER="postgres.fivsbshaikmfjbbxkzsw"
-            DB_PASS="fluAGocL20YYJ7CW"
-            DB_POOLMODE="session"
-            ;;
-        "new.vision.echoit.co.kr")
             DB_HOST="aws-1-ap-northeast-2.pooler.supabase.com"      # - 에코아이티 계정
             DB_PORT="5432"  # ← Session pooler
             DB_DATABASE="postgres"
@@ -21,6 +13,14 @@ get_db_config() {
             DB_PASS="DZiF3eWwyA1fn27C"
             DB_POOLMODE="session"
             ;;
+        # "new.vision.echoit.co.kr")
+        #     DB_HOST="aws-1-ap-northeast-2.pooler.supabase.com"      # - 에코아이티 계정
+        #     DB_PORT="5432"  # ← Session pooler
+        #     DB_DATABASE="postgres"
+        #     DB_USER="postgres.yujniegzuxtyuasrbnvy"
+        #     DB_PASS="DZiF3eWwyA1fn27C"
+        #     DB_POOLMODE="session"
+        #     ;;
         *)
             echo "Unknown project: $PROJECT"
             return 1
